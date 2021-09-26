71d947cef7d602882a1fcc1826eef47d90b35259
Michael Schlueter <schluet0r4life@gmail.com>
2021-09-26 18:00:33 +0200

    added navigation for separate screens

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AllScreen from './screens/AllScreen';
+import BusinessScreen from './screens/BusinessScreen';
+import HealthScreen from './screens/HealthScreen';
+import SportsScreen from './screens/SportsScreen';
+import TechScreen from './screens/TechScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="All" component={AllScreen} />
+        <Tab.Screen name="Business" component={BusinessScreen} />
+        <Tab.Screen name="Health" component={HealthScreen} />
+        <Tab.Screen name="Sports" component={SportsScreen} />
+        <Tab.Screen name="Tech" component={TechScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
