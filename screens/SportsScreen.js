161753608b9369cb69d051cbc02369e807c16e91
Michael Schlueter <schluet0r4life@gmail.com>
2021-09-27 16:44:13 +0200

    added news logic and styling for all other screens

--- screens/SportsScreen.js
+++ screens/SportsScreen.js
@@ -1,24 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { flex } from 'styled-system';
+import { Divider, NativeBaseProvider, FlatList, ScrollView, Image, Spinner } from 'native-base';
+import { services } from '../services/services';
+import moment from 'moment';
 
 export default function SportsScreen() {
+
+    const [newsData, setNewsData] = useState([]);
+    
+    useEffect(() => {
+        services('sports')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, []);
+
     return (
-        <View>
-            <View style={styles.container}>
-                <Text style={styles.text}>Sports</Text>
-            </View>
-        </View>
+        <NativeBaseProvider>
+            <ScrollView height={850}>
+                {newsData.length > 1 ? (
+                <FlatList
+                    data={newsData}
+                    renderItem={({ item }) => (
+                       <View>
+                           <View style={styles.newsContainer}>
+                               <Image
+                                width={550}
+                                height={250}
+                                resizeMode={"cover"}
+                                source={{
+                                    uri: item.urlToImage,
+                                }}
+                                alt="Alternate Text" />
+                                <Text style={styles.title}>
+                                    {item.title}
+                                </Text>
+                                <Text style={styles.date}>
+                                    {moment(item.publishedAt).format('LLL')}
+                                </Text>
+                                <Text style={styles.newsDescription}>
+                                    {item.description}
+                                </Text>
+                            </View>
+                            <Divider my={2} bg="#e0e0e0" />
+                       </View> 
+                    )}
+                    keyExtractor={(item) => item.id}
+                />
+                ) : (
+                    <View style={styles.spinner}>
+                        <Spinner color="danger.400" />
+                    </View>
+                )}
+            </ScrollView>
+        </NativeBaseProvider>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        textAlign: 'center',
+    newsContainer: {
         padding: 10,
-        backgroundColor: '#e0e0e0',
-        border: '1px solid #bdbdbd',
     },
-    text: {
-        fontSize: 24,
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600",
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10,
+    },
+    date: {
+        fontSize: 14,
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 400,
     }
 });
